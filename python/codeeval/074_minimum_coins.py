8dcad73bc0a88409192e813c0a66a73a8d7a6bb2
Cyd La Luz <quetzaluz@gmail.com>
2016-12-26 21:29:34 -0800

    python solution for codeeval #74

---
+++ python/codeeval/074_minimum_coins.py
@@ -0,0 +1,17 @@
+import sys
+
+def mc(test):
+    t = int(test)
+    c = 0
+    d = [5, 3, 1]
+    while t > 0:
+        for i in d:
+            d_i = t // i
+            if d_i > 0:
+                c += d_i
+                t -= (d_i * i)
+    return c
+
+with open(sys.argv[1], 'r') as test_cases:
+    for test in test_cases:
+        print(mc(test))
