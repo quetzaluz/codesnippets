47b1aaf3c07b3378c7e53c5181ad6f5649b15dde
Cyd La Luz <quetzaluz@gmail.com>
2017-02-15 06:15:44 -0800

    Revisions to ruby solution to codeeval #70 - Honestly previous version is more readable; performance gains with this solution are very marginal.

--- ruby/codeeval/070_overlapping_rectangles.rb
+++ ruby/codeeval/070_overlapping_rectangles.rb
@@ -1,14 +1,11 @@
 def ol(line)
     a = line.split(',').map(&:to_i)
-    b1 = a[0..3] 
-    b2 = a[4..-1]
-    if ((b1[0] > b2[2] || b2[0] > b1[2]) || (b1[1] < b2[3] || b2[1] < b1[3]))
+    if ((a[0] > a[6] || a[4] > a[2]) || (a[1] < a[7] || a[5] < a[3]))
         return 'False'
     else
         return 'True'
     end
 end
-
 
 File.open(ARGV[0]).each_line do |line|
     puts(ol(line))
