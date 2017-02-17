c806ec95cfbd3641c5169bd6e975d32ac522f7d2
Cyd La Luz <quetzaluz@gmail.com>
2017-02-17 07:13:54 -0800

    Python solution to codeeval #10

--- python/codeeval/010_mth_to_last.py
+++ python/codeeval/010_mth_to_last.py
@@ -1,7 +1,13 @@
-def m(test):
-    print(test.split(' ')[-2])
-    
 import sys
+
+def m(test):
+    l = test.split(' ')
+    i = int(l.pop())
+    idx = len(l) - i
+    if idx >= 0:
+        v = l[idx]
+        if (v is not None):
+            print(v)
 
 with open(sys.argv[1], 'r') as test_cases:
     for test in test_cases:
