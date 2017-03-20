d83d5c8b6558517f4f35abc80c2296a995a42683
Cyd La Luz <quetzaluz@gmail.com>
2017-03-20 06:37:02 -0700

    Full ruby solution to codeeval #28

--- ruby/codeeval/028_string_search.rb
+++ ruby/codeeval/028_string_search.rb
@@ -1,7 +1,13 @@
-=begin
- Sample code to read in test cases:
-File.open(ARGV[0]).each_line do |line|
-# Do something with line, ignore empty lines
-#...
+def ss(line)
+    t = line.strip().split(',')
+    i = t[0]
+    s = t[1]
+    se = s.gsub('*', '.*')
+    se = se.gsub('\.*', '\*')
+    m = i =~ Regexp.new(se)
+    return !!m
 end
-=end
+
+File.open(ARGV[0]).each_line do |line|
+    puts(ss(line))
+end
