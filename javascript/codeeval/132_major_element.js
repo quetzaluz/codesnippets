562951abc23ee8afffbe3ac5141369663273de93
Cyd La Luz <quetzaluz@gmail.com>
2017-03-30 06:40:57 -0700

    Improvements to javascript codeeval #132

--- javascript/codeeval/132_major_element.js
+++ javascript/codeeval/132_major_element.js
@@ -4,16 +4,15 @@ function me(test) {
     a = test.split(',')
     l = a.length / 2
     c = {}
-    m = a[0]
     for (var i = 0; i < a.length; i++) {
         if (c[a[i]]) {
             c[a[i]] += 1
-            if (c[a[i]] > c[m]) { m = a[i]}
+            if (c[a[i]] > l) { return a[i] }
         } else {
             c[a[i]] = 1
         }
     }
-    return c[m] > l ? m : 'None'
+    return 'None'
 }
 
 fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
