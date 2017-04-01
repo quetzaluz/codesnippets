9e1ee13aef037ce661caaa0706b65530f65aca59
Cyd La Luz <quetzaluz@gmail.com>
2017-04-01 10:02:32 -0700

    Improved python solution to codeeval #132

--- python/codeeval/132_major_element.py
+++ python/codeeval/132_major_element.py
@@ -4,15 +4,14 @@ def me(test):
     a = test.split(',')
     l = len(a) // 2
     c = {}
-    m = a[0]
     for i in a:
         if i in c:
             c[i] += 1
-            if c[i] > c[m]:
-                m = i
+            if c[i] >= l:
+                return i
         else:
             c[i] = 1
-    return m if c[m] >= l else None
+    return None
 
 with open(sys.argv[1], 'r') as test_cases:
     for test in test_cases:
