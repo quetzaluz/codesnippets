c56d25486002fd45fe38763b4755b5930084a370
Cyd La Luz <quetzaluz@gmail.com>
2017-04-11 07:29:16 -0700

    Alternative python solution to codeeval #101

--- python/codeeval/101_find_a_square.py
+++ python/codeeval/101_find_a_square.py
@@ -1,22 +1,21 @@
 import sys
+import math
 
 def ds(p1, p2):
-    return (p1[0] - p2[0])*(p1[0] - p2[0]) + (p1[1] - p2[1])*(p1[1] - p2[1])
+    return math.sqrt(math.pow(p1[0] - p2[0], 2) + math.pow(p1[1] - p2[1], 2))
 
 def find_s(test):
     x = [[int(z) for z in y.split(',')] for y in test.rstrip()[1:-1].split('), (')]
-    d1 = ds(x[0], x[1])
-    d2 = ds(x[0], x[2])
-    d3 = ds(x[0], x[3])
-    if (d1 == d2 and 2*d1 == d3):
-        d = ds(x[1], x[3])
-        return 'true' if (a and d == ds(x[2], x[3]) and d == d1) else 'false'
-    if (d1 == d3 and 2*d1 == d2):
-        d = ds(x[1], x[2])
-        return 'true' if (a and d == ds(x[2], x[3]) and d == d1) else 'false'
-    if (d2 == d3 and 2*d2 == d1):
-        d = ds(x[1], x[2])
-        return 'true' if (a and d == ds(x[1], x[3]) and d == d2) else 'false'
+    d = []
+    d.append(ds(x[0], x[1]))
+    d.append(ds(x[1], x[2]))
+    d.append(ds(x[2], x[3]))
+    d.append(ds(x[3], x[0]))
+    d.append(ds(x[0], x[2]))
+    d.append(ds(x[1], x[3]))
+    d.sort()
+    if d[0] == d[1] and d[0] == d[2] and d[0] == d[3] and d[4] == d[5]:
+        return 'true'
     return 'false'
 
 with open(sys.argv[1], 'r') as test_cases:
