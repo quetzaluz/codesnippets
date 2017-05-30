4201ad2d5e3b96df6dacedeae240995f4b550dfc
Cyd La Luz <quetzaluz@gmail.com>
2017-05-29 20:22:57 -0700

    javascript solution to codeeval #235

--- javascript/codeeval/235_trump_card.js
+++ javascript/codeeval/235_trump_card.js
@@ -3,19 +3,23 @@ var fs  = require("fs");
 ORDER = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 function st(inp) {
-    inps = inp.replace(/\n$/, '').split(' | ')
+    inps = inp.split(' | ')
     s = inps[1]
     cs = inps[0].split(' ')
-    c2 = cs.pop()
-    c1 = cs.pop()
-    if (c1[c1.length - 1] == s) {
-        return c1
+    c1 = cs[0]
+    c2 = cs[1]
+    c1l = c1.length - 1
+    c2l = c2.length - 1
+    if (c1[c1l] == s) {
+        if (c2[c2l] != s) {
+            return c1
+        }
     }
-    else if (c2[c2.length - 1] == s) {
+    else if (c2[c2l] == s) {
         return c2
     }
-    c1i = ORDER.indexOf(c1.substr(0,c1.length - 1))
-    c2i = ORDER.indexOf(c2.substr(0,c2.length - 1))
+    c1i = ORDER.indexOf(c1.substr(0,c1l))
+    c2i = ORDER.indexOf(c2.substr(0,c2l))
     if (c1i > c2i) {
         return c1
     }
