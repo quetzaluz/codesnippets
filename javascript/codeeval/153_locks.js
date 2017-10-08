eb7ea22ff460d1d5e6a03c594991934e6719cc6b
Cyd La Luz <quetzaluz@gmail.com>
2017-10-07 20:01:52 -0700

    Revisions to javascript solution to codeeval 153

--- javascript/codeeval/153_locks.js
+++ javascript/codeeval/153_locks.js
@@ -3,16 +3,25 @@ function doors(inp) {
   var d = parseInt(inp[0])
   var ite = parseInt(inp[1])
   var r = []
-  for (var i = 0; i < ite.length; i++) {
+  for (var i = 0; i < ite; i++) {
     for (var t = 0; t < 2; t++) {
       var mod = t + 2;
-      for (var j = 0; j < d.length; j++) {
+      var modeq = t + 1
+      for (var j = 0; j < d; j++) {
         if (t == 0 && i == 0) {
           r.push(j % 2)
         } else {
-          // check door and open/close depending on modulo match
+          if (j % mod == modeq) {
+            r[j] = r[j] == 1 ? 0 : 1
+          }
         }
       }
     }
   }
+
+  result = 0
+  for (var i = 0; i < d; i++) {
+    result += r[i]
+  }
+  return result
 }
