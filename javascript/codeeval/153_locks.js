3ca7890465d134555b5f1552f9dca6bb5f6fb29f
Cyd La Luz <quetzaluz@gmail.com>
2017-10-10 06:49:01 -0700

    solution for codeeval 153 javascript (passes 90% of cases)

--- javascript/codeeval/153_locks.js
+++ javascript/codeeval/153_locks.js
@@ -1,3 +1,4 @@
+var fs  = require("fs");
 function doors(inp) {
   inp = inp.split(' ')
   var d = parseInt(inp[0])
@@ -8,15 +9,16 @@ function doors(inp) {
   }
   for (var i = 0; i < ite; i++) {
     for (var j = 0; j < d; j++) {
-      mod2 = j % 2
-      mod3 = j % 3
-      if (mod2 == 1 && mod3 == 2) {
+      var n = j + 1
+      mod2 = n % 2 == 0
+      mod3 = n % 3 == 0
+      if (mod2 && mod3) {
         r[j] = 1
       }
-      else if (mod2 == 1) {
+      else if (mod2) {
         r[j] = 0
       }
-      else if (mod3 == 2) {
+      else if (mod3) {
         r[j] = 0
       }
     }
@@ -30,3 +32,9 @@ function doors(inp) {
   }
   return result
 }
+
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+    if (line != "") {
+        console.log(doors(line))
+    }
+});
