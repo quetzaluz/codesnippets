67af104628da983a7073f327f1be7ff742c7ed9c
Cyd La Luz <quetzaluz@gmail.com>
2017-10-25 06:37:46 -0700

    Full python solution to codeeval #153

--- python/codeeval/153_locks.py
+++ python/codeeval/153_locks.py
@@ -3,18 +3,17 @@ import sys
 def doors(inp):
 	inp = inp.split(' ')
 	d = int(inp[0])
-	ite = int(inp[1])
+	ite = (int(inp[1]) * 2) - 1
 	r = [1] * d
 	for i in range(0, ite):
 		for j in range(0, d):
-			n = j + 1
-			mod2 = n % 2 == 0
-			mod3 = n % 3 == 0
-			if mod2 and mod3:
-				r[j] = 1
-			elif mod2 or mod3:
+			if i == ite - 1:
+				r[d-1] = 0 if r[d-1] == 1 else 1
+				break
+			elif i % 2 == 0 and j % 2 == 1:
 				r[j] = 0
-	r[d-1] = 0 if r[d-1] == 1 else 1
+			elif i % 2 == 1 and j % 3 == 2:
+				r[j] = 0 if r[j] == 1 else 1
 	result = 0
 	for i in range(0, d):
 		result += r[i]
