b1353974707e77442d16e3af034e093480a4f67e
Cyd La Luz <quetzaluz@gmail.com>
2017-10-26 06:49:48 -0700

    javascript solution to codeeval #153

--- javascript/codeeval/153_locks.js
+++ javascript/codeeval/153_locks.js
@@ -2,29 +2,24 @@ var fs  = require("fs");
 function doors(inp) {
   inp = inp.split(' ')
   var d = parseInt(inp[0])
-  var ite = parseInt(inp[1])
+  var ite = (parseInt(inp[1]) * 2) - 1
   var r = []
   for (var j = 0; j < d; j++) {
     r.push(1)
   }
   for (var i = 0; i < ite; i++) {
     for (var j = 0; j < d; j++) {
-      var n = j + 1
-      mod2 = n % 2 == 0
-      mod3 = n % 3 == 0
-      if (mod2 && mod3) {
-        r[j] = 1
-      }
-      else if (mod2) {
+      if (i == ite - 1) {
+        r[d-1] = r[d-1] == 1 ? 0 : 1
+        break
+      } else if (i % 2 == 0 && j % 2 == 1) {
         r[j] = 0
       }
-      else if (mod3) {
-        r[j] = 0
+      else if (i % 2 == 1 && j % 3 == 2) {
+        r[j] = r[j] == 1 ? 0 : 1
       }
     }
   }
-
-  r[d-1] = r[d-1] == 1 ? 0 : 1
 
   result = 0
   for (var i = 0; i < d; i++) {
