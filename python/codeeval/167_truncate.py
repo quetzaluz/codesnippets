20385388f2b8c1b11c6a5d2f3979d207f84f7596
Cyd La Luz <quetzaluz@gmail.com>
2017-10-31 06:35:01 -0700

    Initial work on python solution to codeeval #167 - passes 98% of cases

---
+++ python/codeeval/167_truncate.py
@@ -0,0 +1,17 @@
+import sys
+
+def trunc(inp):
+    if len(inp) > 55:
+        new_str = inp[0:40]
+        try:
+            last_space = len(new_str) - 1 - new_str[::-1].index(' ')
+            new_str = new_str[0:last_space]
+        except ValueError:
+            pass
+        return new_str + '... <Read More>'
+    else:
+        return inp
+
+with open(sys.argv[1], 'r') as test_cases:
+    for test in test_cases:
+        print(trunc(test))
