2c5405caad245b5a13d39b129c1417e56a94f8ce
Cyd La Luz <quetzaluz@gmail.com>
2017-11-07 06:07:47 -0800

    Full answer to question for creating an array of products.

--- javascript/google_pactice/product_of_all_but_self.js
+++ javascript/google_pactice/product_of_all_but_self.js
@@ -1,7 +1,14 @@
 function productOfAllButSelf (arr, n) {
-	var accum = 1;
-	for (var i = 0; i < arr.length; i++) {
-		if (arr[i] != n) { accum *= arr[i] }
+	var b = [1] // Initialize because we are going to be adding right side products first
+	var a = 1
+	for (var i = 0; i <= arr.length - 2; i++) {
+		a *= arr[i]
+		b[i+1] = a
 	}
-	return accum;
+	a = 1
+	for (var i = (arr.length - 1); i >= 1; i--) {
+		a *= arr[i]
+		b[i - 1] *= a
+	}
+	return b
 }
