58eec1d3fb99529599950efec6e7436c10fd69a8
Cyd La Luz <quetzaluz@gmail.com>
2017-11-16 07:46:47 -0800

    Separate methods in counting bits solution; longer runtime but more elegant

--- javascript/leetcode/counting-bits.js
+++ javascript/leetcode/counting-bits.js
@@ -5,13 +5,17 @@
 var countBits = function(num) {
     var result = []
     for (var i = 0; i <= num; i++) {
-        var countOne = 0
-        var thisNum = i;
-        while (thisNum) {
-            if (thisNum % 2 !== 0) countOne++
-            thisNum = ~~(thisNum / 2)
-        } 
-        result.push(countOne)
+        result.push(countOnes(i))
     }
     return result
 };
+
+// Helper method that counts one given that binary is a representation of powers of 2
+var countOnes = function (thisNum) {
+    var countOne = 0
+    while (thisNum) {
+        if (thisNum % 2 !== 0) countOne++
+        thisNum = ~~(thisNum / 2)
+    }
+    return countOne
+}
