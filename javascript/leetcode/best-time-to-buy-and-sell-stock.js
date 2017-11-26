0bfa7031f3133eab47df7f7f892e703029c6ce26
Cyd La Luz <quetzaluz@gmail.com>
2017-11-25 17:39:25 -0800

    Javascript solution to leetcode best-time-to-buy-and-sell-stock

---
+++ javascript/leetcode/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    var maxProfit = 0;
+    var min = Infinity;
+    for (var i = 0; i < prices.length; i++) {
+        if (min > prices[i]) { min = prices[i] }
+        if ((prices[i] - min) > biggestDiff) { biggestDiff = (prices[i] - min)}
+    }
+    return biggestDiff;
+};
