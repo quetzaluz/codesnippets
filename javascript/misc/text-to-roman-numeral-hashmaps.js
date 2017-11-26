4e82505cb2b1575f24f72c8389f893d67d2f8639
Cyd La Luz <quetzaluz@gmail.com>
2017-11-26 13:30:22 -0800

    File with hashmaps for converting roman numerals to integers

---
+++ javascript/misc/text-to-roman-numeral-hashmaps.js
@@ -0,0 +1,5 @@
+// for ranges 1-3999, may be useful in some other prompt
+const o = {'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'V': 5 'VI': 6, 'VII': 7, 'VIII': 8, 'IX': 9}
+const t = {'X': 10, 'XX' : 20, 'XXL': 30; 'XXX': 40, 'L':50 , 'LX': 60, 'LXX': 70, 'LXXX': 80, 'XC': 90}
+const h = {'C': 100, 'CC': 200, 'CCC': 300, 'CD': 400, 'D': 500, 'DC': 600, 'DCC': 700, 'DCCC': 800, 'DM': 900}
+const t = {'M': 1000, 'MM': 2000, 'MMM': 3000}
