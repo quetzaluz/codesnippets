cb455e60069be28817702cc7747891bf8ab62ccc
Cyd La Luz <quetzaluz@gmail.com>
2017-12-05 05:45:22 -0800

    Javascript solution to longest-substring-without-repeating-characters

--- javascript/leetcode/longest-substring-without-repeating-characters.js
+++ javascript/leetcode/longest-substring-without-repeating-characters.js
@@ -6,18 +6,16 @@ var lengthOfLongestSubstring = function(s) {
     currentSubstr = ''
     longestSubstr = ''
     lastIdx = 0
-    while (lastIdx < s.length) {
-        for (var i = (lastIdx + 0); i < s.length; i++) {
-            if (currentSubstr.indexOf(s[i]) == -1) {
-                currentSubstr += s[i]
-                lastIdx++
-            } else {
-                if (currentSubstr.length > longestSubstr.length) {
-                    longestSubstr = currentSubstr
-                }
-                currentSubstr = ''
-                break
+    for (var i = (lastIdx + 0); i < s.length; i++) {
+        indexOfExisting = currentSubstr.indexOf(s[i])
+        if (indexOfExisting == -1) {
+            currentSubstr += s[i]
+        } else {
+            if (currentSubstr.length > longestSubstr.length) {
+                longestSubstr = currentSubstr
             }
+            currentSubstr += s[i]
+            currentSubstr = currentSubstr.slice(indexOfExisting+1)
         }
     }
     if (currentSubstr.length > longestSubstr.length)  { longestSubstr = currentSubstr }
