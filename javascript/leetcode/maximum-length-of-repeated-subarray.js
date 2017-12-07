fa1dd5fe79c58a1ad93c9f80569d43e393949585
Cyd La Luz <quetzaluz@gmail.com>
2017-12-07 06:30:04 -0800

    Dynamic programming solution to maximum-length-of-repeated-subarray based on writeup at https://leetcode.com/articles/maximum-length-of-repeated-subarray/

--- javascript/leetcode/maximum-length-of-repeated-subarray.js
+++ javascript/leetcode/maximum-length-of-repeated-subarray.js
@@ -4,25 +4,26 @@
  * @return {number}
  */
 var findLength = function(A, B) {
-    var subArr = []
-    var longestArr = []
-    var indexOfExisting, lastIndexOfExisting;
-    for (var i = 0; i < A.length; i++) {
-        indexOfExisting = B.indexOf(A[i])
-        if (indexOfExisting > -1 && (!lastIndexOfExisting || indexOfExisting == lastIndexOfExisting + 1)) {
-            subArr.push(A[i])
-            lastIndexOfExisting = indexOfExisting
-        } else {
-            if (subArr.length > longestArr.length) {
-                longestArr = subArr
+    memo = []
+    for (var i = 0; i <= A.length; i++) {
+        arr = []
+        for (var j = 0; j <= B.length; j++) {
+            arr.push(0)
+        }
+        memo.push(arr)
+    }
+    for (var i = A.length - 1; i >= 0; i--) {
+        for (var j = B.length - 1; j >= 0; j--) {
+            if (A[i] == B[j]) {
+                memo[i][j] = memo[i+1][j+1]+1
             }
-            subArr.push(A[i])
-            subArr = subArr.slice(indexOfExisting+1)
-            lastIndexOfExisting = null
-            indexOfExisting = null
         }
     }
-    if (subArr.length > longestArr.length)  { longestArr = subArr }
-   	console.log(longestArr)
-    return longestArr.length
+    var max = 0
+    for (var i = 0; i < memo.length; i++) {
+        for (var j = 0; j < memo[i].length; j++) {
+            if (memo[i][j] > max) { max = memo[i][j] }
+        }
+    }
+    return max
 };
