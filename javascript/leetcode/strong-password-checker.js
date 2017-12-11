5fd4dabfc3bbba1807abcdba0c5919591327e0e2
Cyd La Luz <quetzaluz@gmail.com>
2017-12-11 06:55:20 -0800

    Initial work on leetcode strong-password-checker

---
+++ javascript/leetcode/strong-password-checker.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var strongPasswordChecker = function(s) {
+    conditionsNeeded = 5 // length, uppercase, lowercase, digit, no repeat
+    conditionsMet = 0
+    hasStringOfLowercase = false
+    hasLower = 0
+    hasUpper = 0
+    hasDigit = 0
+    hasCorrectLength = s.length >= 6 && s.length <= 20
+    var lastCharaCount = 0
+    var lastChara
+    for (var i = 0; i < s.length; i++) {
+        if (s[i] == lastChara) {
+            lastCharaCount++
+        } else {
+            lastCharaCount = 0
+        }
+        if (lastCharaCount == 3) {hasStringOfLowercase = true}
+        if (isNaN(s[i])) {
+            if (s[i] == s[i].toLowerCase()) {
+                hasLower = 1
+            } else {
+                hasUpper = 1
+            }
+        } else {
+            hasDigit = 1
+        }
+        lastChara = s[i]
+    }
+    conditionsMet = (hasStringOfLowercase ? 0 : 1) + (hasCorrectLength ? 1 : 0) + hasLower + hasUpper + hasDigit
+    if (conditionsMet == conditionsNeeded) {
+        return 0
+    } else if (!hasCorrectLength) {
+        if (s.length < 6) {
+            return Math.max(6 - s.length, conditionsNeeded - conditionsMet)
+        } else {
+            return Math.max(s.length - 20, conditionsNeeded - conditionsMet)
+        }
+    } else {
+        return conditionsNeeded - conditionsMet
+    }
+};
