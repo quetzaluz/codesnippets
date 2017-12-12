30c1cc428b99f657015c4eaaab221c6b99e9d135
Cyd La Luz <quetzaluz@gmail.com>
2017-12-12 06:21:19 -0800

    Full solution to javascript strong password checker modeled after: https://discuss.leetcode.com/topic/63341/simple-python-solution

--- javascript/leetcode/strong-password-checker.js
+++ javascript/leetcode/strong-password-checker.js
@@ -4,24 +4,14 @@
  */
 var strongPasswordChecker = function(s) {
     conditionsNeeded = 3 // uppercase, lowercase, digit
-    conditionsMet = 0
-    consecutiveSequences = 0
     hasLower = 0
     hasUpper = 0
     hasDigit = 0
-    hasCorrectLength = s.length >= 6 && s.length <= 20
-    var lastCharaCount = 0
-    var lastChara
+    change = 0
+    one = 0
+    two = 0
+    p = 2
     for (var i = 0; i < s.length; i++) {
-        if (s[i] == lastChara) {
-            lastCharaCount++
-        } else {
-            lastCharaCount = 1
-        }
-        if (lastCharaCount == 3) {
-            lastCharaCount = 0
-            consecutiveSequences++
-        }
         if (isNaN(s[i])) {
             if (s[i] == s[i].toLowerCase()) {
                 hasLower = 1
@@ -31,18 +21,31 @@ var strongPasswordChecker = function(s) {
         } else {
             hasDigit = 1
         }
-        lastChara = s[i]
     }
-    conditionsMet = hasLower + hasUpper + hasDigit
-    if (hasCorrectLength && !consecutiveSequences && conditionsMet == conditionsNeeded) {
-        return 0
-    } else if (!hasCorrectLength) {
-        if (s.length < 6) {
-            return Math.max(6 - s.length, Math.max(conditionsNeeded - conditionsMet, consecutiveSequences))
+    conditionsMet = hasDigit + hasLower + hasUpper
+    while (p < s.length) {
+        if (s[p] == s[p-1] && s[p] == s[p-2]) {
+            length = 2
+            while (p < s.length && s[p] == s[p-1]) {
+                length++
+                p++
+            }
+            change += Math.floor(length / 3)
+            if (length % 3 == 0) { one++ }
+            else if (length % 3 == 1) { two++ }
         } else {
-            return (s.length - 20) + Math.max(conditionsNeeded - conditionsMet, consecutiveSequences - (s.length - 20))
+            p++
         }
+    }
+    if (s.length < 6) {
+        return Math.max(conditionsNeeded - conditionsMet, 6 - s.length)
+    } else if (s.length <= 20) {
+        return Math.max(conditionsNeeded - conditionsMet, change)
     } else {
-        return Math.max(conditionsNeeded - conditionsMet, consecutiveSequences)
+        toDelete = s.length - 20
+        change -= Math.min(toDelete, one)
+        change -= Math.floor(Math.min(Math.max(toDelete - one, 0), two * 2) / 2)
+        change -= Math.floor(Math.max(toDelete - one - 2 * two, 0) / 3)
+        return toDelete + Math.max(conditionsNeeded - conditionsMet, change)
     }
 };
