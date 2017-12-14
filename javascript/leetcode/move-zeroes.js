d3f961f9c4214ebd417fcff956e17f505c922f12
Cyd La Luz <quetzaluz@gmail.com>
2017-12-14 06:11:07 -0800

    Working javascript solution to move-zeroes.

--- javascript/leetcode/move-zeroes.js
+++ javascript/leetcode/move-zeroes.js
@@ -3,15 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    var openIndexes = []
-    for (var i = 0; i <= nums.length; i++) {
-        if (nums[i] == 0) {
-            openIndexes.push(i)
-        } else if (nums[i] == null) {
-            nums[i] = 0
-        } else if (openIndexes.length > 0) {
-            nums[openIndexes.shift()] = nums[i]
-            nums[i] = null;
+    lastIdx = 0
+    for (var i = 0; i < nums.length; i++) {
+        if (nums[i] != 0) {
+            nums[lastIdx++] = nums[i]
         }
+    }
+    while (lastIdx < nums.length) {
+        nums[lastIdx++] = 0
     }
 };
