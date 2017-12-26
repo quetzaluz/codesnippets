81681ddfc40c123836a612db09c9d8b9170f56c7
Cyd La Luz <quetzaluz@gmail.com>
2017-12-25 19:05:02 -0800

    More accurate but inefficient solution to codeeval #06

--- javascript/codeeval/006_longest_common_subsequence.js
+++ javascript/codeeval/006_longest_common_subsequence.js
@@ -10,6 +10,27 @@ var findSubsequence = function (s1, s2) {
         if (s1[i] == s2[j]) {
           thisSubsequence += s1[i]
           lastIdx = j
+          break
+        }
+        j++
+      }
+    }
+    if (thisSubsequence.length > longestSubsequence.length) {
+      longestSubsequence = thisSubsequence
+    }
+    startIdx++
+  }
+  startIdx = 0
+  while (startIdx < s2.length) {
+    thisSubsequence = ''
+    lastIdx = 0
+    for (var i = startIdx; i < s2.length; i++) {
+      j = lastIdx
+      while (j < s1.length) {
+        if (s2[i] == s1[j]) {
+          thisSubsequence += s2[i]
+          lastIdx = j
+          break
         }
         j++
       }
