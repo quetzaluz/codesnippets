bfa0da910ab86123f294b745872575dfbc2c20fa
Cyd La Luz <quetzaluz@gmail.com>
2018-01-24 07:30:58 -0800

    Javascript solution to leetcode pascals-triangle-ii

---
+++ javascript/leetcode/pascals-triangle-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+var getRow = function(rowIndex) {
+    iterations = 0
+    result = []
+    while (iterations < rowIndex + 1) {
+        result.push(generateRow(result[iterations - 1]))
+        iterations++
+    }
+    console.log(result)
+    return result[rowIndex] 
+};
+
+var generateRow = function(lastRow) {
+    if (!lastRow) {return [1]}
+    newRow = []
+    newRow.push(lastRow[0])
+    for (var i = 0; i < lastRow.length - 1; i++) {
+        newRow.push(lastRow[i] + lastRow[i + 1])
+    }
+    newRow.push(lastRow[lastRow.length -1])
+    return newRow
+}
