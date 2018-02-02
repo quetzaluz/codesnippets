3fb7ba2f8996fa3c5ad412a90da0b2e2591e0d87
Cyd La Luz <quetzaluz@gmail.com>
2018-02-02 06:46:37 -0800

    Working solution to leetcode valid-parentheses - Can benefit from less repetition, removal of redundant cnt var. - Also can be ported over to codeeval which had less thorough cases

--- javascript/leetcode/valid-parentheses.js
+++ javascript/leetcode/valid-parentheses.js
@@ -4,40 +4,45 @@
  */
  // current failing case - "[([]])"
 var isValid = function(s) {
-	v = {p: 0, b: 0, c: 0}
-	prev = ''
-	for (var i = 0; i <= s.length; i++) {
-		l = s[i]
+    v = {p: {idx: [], cnt: 0}, s: {idx: [], cnt: 0}, c: {idx: [], cnt: 0}}
+    for (var i = 0; i <= s.length; i++) {
+        l = s[i]
         if (l == '(') {
-            v['p'] += 1
-		} else if (l == ')') {
-            if (prev == '' || prev == '[' || prev == '{') {
+            v['p'].cnt += 1
+            v['p'].idx.push(i)
+        } else if (l == ')') {
+            if (v['p'].cnt == 0 || v['s'].idx[v['s'].idx.length - 1] > v['p'].idx[v['p'].idx.length - 1] || v['c'].idx[v['c'].idx.length - 1] > v['p'].idx[v['p'].idx.length - 1]) {
                 return false
             } else {
-                v['p'] -= 1
-			}
+                v['p'].cnt -= 1
+                v['p'].idx.pop()
+            }
         } else if (l == '[') {
-            v['b'] += 1
+            v['s'].cnt += 1
+            v['s'].idx.push(i)
         } else if (l == ']') {
-            if (prev == '' || prev == '(' || prev == '{') {
+            if (v['s'].cnt == 0 || v['p'].idx[v['p'].idx.length - 1] > v['s'].idx[v['s'].idx.length - 1] || v['c'].idx[v['c'].idx.length - 1] > v['s'].idx[v['s'].idx.length - 1]) {
                 return false
             } else {
-                v['b'] -= 1
+                v['s'].cnt -= 1
+                v['s'].idx.pop()
             }
         } else if (l == '{') {
-            v['c'] += 1
+            v['c'].cnt += 1
+            v['c'].idx.push(i)
         } else if (l == '}') {
-            if (prev == '' || prev == '[' || prev == '(') {
+            if (v['c'].cnt == 0 ||  v['p'].idx[v['p'].idx.length - 1] > v['c'].idx[v['c'].idx.length - 1] || v['s'].idx[v['s'].idx.length - 1] > v['c'].idx[v['c'].idx.length - 1]) {
                 return false
             } else {
-                v['c'] -= 1
+                v['c'].cnt -= 1
+                v['c'].idx.pop()
             }
         }
         prev = l
     }
-    if (v['p'] != 0 || v['b'] != 0 || v['c'] != 0) {
+    if (v['p'].cnt != 0 || v['s'].cnt != 0 || v['c'].cnt != 0) {
         return false
     } else {
         return true
-	}
+    }
 };
