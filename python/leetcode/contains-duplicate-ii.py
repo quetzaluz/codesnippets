63e4ac3f3434d324bbff2143d9ac3e404519653b
Cyd La Luz <quetzaluz@gmail.com>
2018-02-23 06:44:20 -0800

    Python solution to leetcode contains-duplicate-ii (nearby duplicate)

---
+++ python/leetcode/contains-duplicate-ii.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        seen = {}
+        for i in range(len(nums)):
+            if nums[i] in seen and i - seen[nums[i]] <= k:
+                return True
+            seen[nums[i]] = i
+        return False
