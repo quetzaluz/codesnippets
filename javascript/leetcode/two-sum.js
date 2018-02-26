6bdecbc7d5d46b488e6d96a387a8a5d1472ace2a
Cyd La Luz <quetzaluz@gmail.com>
2018-02-26 06:36:13 -0800

    Javascript solution to leetcode two-sum that runs in linear time

--- javascript/leetcode/two-sum.js
+++ javascript/leetcode/two-sum.js
@@ -3,14 +3,13 @@
  * @param {number} target
  * @return {number[]}
  */
-// O(n^2) below, should be able to optimize
 var twoSum = function(nums, target) {
+    seen = {}
     for (var i = 0; i < nums.length; i++) {
-        for (var j = 0; j < nums.length; j++) {
-            if (j !== i && nums[i] + nums[j] === target) {
-                return [i, j]
-            }
+        if (typeof seen[target - nums[i]] !== 'undefined') {
+            return [i, seen[target - nums[i]]]
         }
+        seen[nums[i]] = i
     }
     return false
 };
