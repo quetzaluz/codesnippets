bc2a263831f05429345ef05a76591de7107470af
Cyd La Luz <quetzaluz@gmail.com>
2018-03-01 05:55:57 -0800

    Initial python solution to leetcode maximum-subarray - works but can time out on very large arrays

---
+++ python/leetcode/maximum-subarray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxSubArray(self, nums):
+        start_idx = 0
+        max_s = nums[start_idx]
+        while(start_idx < len(nums)):
+            this_sum = nums[start_idx]
+            if (this_sum > max_s):
+                max_s = this_sum
+            for i in range(start_idx + 1, len(nums)):
+                this_sum += nums[i]
+                if this_sum > max_s:
+                    max_s = this_sum
+            start_idx += 1
+        return max_s
