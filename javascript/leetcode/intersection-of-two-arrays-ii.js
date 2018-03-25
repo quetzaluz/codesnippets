071274f9549f050296277d7ae149666d7ab12503
Cyd La Luz <quetzaluz@gmail.com>
2018-03-25 11:29:59 -0700

    Complete javascript solution to intersection-of-two-arrays-ii

--- javascript/leetcode/intersection-of-two-arrays-ii.js
+++ javascript/leetcode/intersection-of-two-arrays-ii.js
@@ -7,18 +7,20 @@ var intersect = function(nums1, nums2) {
     newArr = []
     seen = {}
     for (var i = 0; i < nums1.length; i++) {
-        for (var j = 0; j < nums2.length; j++) {
-            if (nums2[j] == nums1[i]) {
-            	if (seen[nums2[j]]) {
-	                seen[nums2[j]]['ct2']++
-            	} else {
-	                seen[nums2[j]] = {'ct1': 0, 'ct2': 1}
-            	}
-            }
+        if (seen[nums1[i]]) {
+            seen[nums1[i]]['ct1']++
+        } else {
+            seen[nums1[i]] = {'ct1': 1, 'ct2': 0}
         }
-        if (seen[nums1[i]]) { seen[nums1[i]]['ct1']++ }
     }
-	console.log(seen)
+    for (var i = 0; i < nums2.length; i++) {
+        if (seen[nums2[i]]) {
+            seen[nums2[i]]['ct2']++
+        } else {
+            seen[nums2[i]] = {'ct1': 0, 'ct2': 1}
+        }
+    }
+    console.log(seen)
     for (key in seen) {
         for (var j = 0; j < Math.min(seen[key]['ct1'], seen[key]['ct2']); j++) {
             newArr.push(parseInt(key))
