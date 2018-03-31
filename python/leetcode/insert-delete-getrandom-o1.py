40d3cc16473049ca7c6521107f4d523a6b32ebc6
Cyd La Luz <quetzaluz@gmail.com>
2018-03-31 14:26:42 -0700

    Python solution to leetcode insert-delete-getrandom-o1

---
+++ python/leetcode/insert-delete-getrandom-o1.py
@@ -0,0 +1,50 @@
+class RandomizedSet:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.set = []
+        self.length = 0
+
+    def insert(self, val):
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        for n in self.set:
+            if n == val:
+                return False
+        self.set.append(val)
+        self.length += 1
+        return True
+
+    def remove(self, val):
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        :type val: int
+        :rtype: bool
+        """
+        for i in range(self.length):
+            if self.set[i] == val:
+                self.length -= 1
+                del self.set[i]
+                return True
+        return False
+        
+
+    def getRandom(self):
+        """
+        Get a random element from the set.
+        :rtype: int
+        """
+        return self.set[int(random.random() * self.length // 1)]
+        
+
+
+# Your RandomizedSet object will be instantiated and called as such:
+# obj = RandomizedSet()
+# param_1 = obj.insert(val)
+# param_2 = obj.remove(val)
+# param_3 = obj.getRandom()
