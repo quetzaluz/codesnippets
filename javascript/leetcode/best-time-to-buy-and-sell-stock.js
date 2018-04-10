0583e40133213878010a49e214be9c0c14665e19
Cyd La Luz <quetzaluz@gmail.com>
2018-04-10 06:33:16 -0700

    Initialize biggestDiff in javascript stock problem to handle empty array edgecase

--- javascript/leetcode/best-time-to-buy-and-sell-stock.js
+++ javascript/leetcode/best-time-to-buy-and-sell-stock.js
@@ -5,6 +5,7 @@
 var maxProfit = function(prices) {
     var maxProfit = 0;
     var min = Infinity;
+    var biggestDiff = 0;
     for (var i = 0; i < prices.length; i++) {
         if (min > prices[i]) { min = prices[i] }
         if ((prices[i] - min) > biggestDiff) { biggestDiff = (prices[i] - min)}
