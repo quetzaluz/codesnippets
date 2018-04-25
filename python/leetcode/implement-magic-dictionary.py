7b98097bfb8b97940eeaf0a38e56b02f9b48294d
Cyd La Luz <quetzaluz@gmail.com>
2018-04-25 07:03:44 -0700

    Complete python solution to leetcode implement-magic-dictionary

--- python/leetcode/implement-magic-dictionary.py
+++ python/leetcode/implement-magic-dictionary.py
@@ -6,11 +6,6 @@ class MagicDictionary:
         """
         self.lookup = []
         
-    def buildLetterFrequency(self, word):
-        freq = {}
-        for c in word:
-            freq[c] = 1 if c not in freq else freq[c] + 1
-        return freq
 
     def buildDict(self, dict):
         """
@@ -19,7 +14,7 @@ class MagicDictionary:
         :rtype: void
         """
         for word in dict:
-            self.lookup.append({'word': word, 'len': len(word), 'freq': self. buildLetterFrequency(word)})        
+            self.lookup.append(word)      
 
     def search(self, word):
         """
@@ -27,30 +22,18 @@ class MagicDictionary:
         :type word: str
         :rtype: bool
         """
-        letter_freq = self.buildLetterFrequency(word)
-        found_word = False
         for w in self.lookup:
-            if w['len'] != len(word):
+            if w == word or len(w) != len(word):
                 continue
-            if w['word'] == word:
-                continue
-            missing = 0
-            extra = 0
-            for c in letter_freq:
-                if c not in w['freq']:
-                    extra += letter_freq[c]
-                else:
-                    if w['freq'][c] > letter_freq[c]:
-                        missing += w['freq'][c] - letter_freq[c]
-                    else:
-                        extra += letter_freq[c] - w['freq'][c]
-            for c in w['freq']:
-                if c not in letter_freq:
-                    missing += w['freq'][c]
-            if missing <= 1 and extra <= 1 and (extra - missing == 0):
-                found_word = True
-                break
-        return found_word
+            diff = 0
+            for i in range(len(word)):
+                if word[i] != w[i]:
+                    diff += 1
+                if diff > 1:
+                    break
+            if diff == 1:
+                return True
+        return False
 
 # Your MagicDictionary object will be instantiated and called as such:
 # obj = MagicDictionary()
