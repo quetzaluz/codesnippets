4fc19d2a381fa0d9d3aed21a67b11e6ccafb4bb2
Cyd La Luz <quetzaluz@gmail.com>
2018-05-01 07:15:08 -0700

    Javascript solution to powx-n that can handle negative exponents

--- javascript/leetcode/powx-n.js
+++ javascript/leetcode/powx-n.js
@@ -3,12 +3,21 @@
  * @param {number} n
  * @return {number}
  */
-var myPow = function(x, n) {
+
+var pow = function(x, n) {
     if (n == 0) {
         return 1
-    } else if (n % 2 === 0) {
-        return  myPow(x, parseInt(n / 2)) *  myPow(x, parseInt(n / 2))
-    } else {
-        return x *  myPow(x, parseInt(n / 2)) *  myPow(x, parseInt(n / 2))
     }
+    partialPow = pow(x, parseInt(n / 2))
+    if (n % 2 === 0) {
+        res = partialPow * partialPow
+    } else {
+        res = x * partialPow * partialPow
+    }
+    return res
+};
+
+var myPow = function(x, n) {
+    res = pow(x, n < 0 ? n * - 1 : n)
+    return n < 0 ? 1 / res : res
 };
