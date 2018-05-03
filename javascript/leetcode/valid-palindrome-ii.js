96449f9414f449bdf3656889a8dba496f2b1061a
Cyd La Luz <quetzaluz@gmail.com>
2018-05-02 19:48:31 -0700

    Complete javascript solution to leetcode valid-palindrome-ii

--- javascript/leetcode/valid-palindrome-ii.js
+++ javascript/leetcode/valid-palindrome-ii.js
@@ -3,33 +3,24 @@
  * @return {boolean}
  */
 
-var validPalindrome = function(s, skippedOnce) {
-    if (s == '') { return true }
-    p1 = 0
-    p2 = s.length - 1
-    s = s.toLowerCase()
-    while (p1 < p2) {
-        if (s[p1] !== s[p2]) {
-            if (skippedOnce || (s[p1] !== s[p2 - 1] && s[p1 + 1] !== s[p2])) {
-                return false
-            } else {
-                skippedOnce = true
-            }
-            if (s[p1] === s[p2 - 1]) {
-                if (validPalindrome(s.slice(p1, p2), true)) {
-                    return true
-                }
-            }
-            if (s[p1 + 1] === s[p2]) {
-                if(validPalindrome(s.slice(p1 + 1, p2 + 1), true)) {
-                    return true
-                }
-            }
+var validPalindromSubsequence = function(p1, p2, s) {
+    for (var k = p1; k < p2; k++) {
+        if (s[k] !== s[p2 - k + p1]) {
             return false
-        }  else {
-            p1++
-            p2--
         }
+    }
+    return true
+}
+
+var validPalindrome = function(s) {
+    p1 = 0
+    s = s.toLowerCase()
+    while (p1 < Math.floor(s.length / 2)) {
+        p2 = s.length - p1 - 1
+        if (s[p1] !== s[p2]) {
+            return validPalindromSubsequence(p1 + 1, p2, s) || validPalindromSubsequence(p1, p2 - 1, s)
+        }
+        p1++
     }
     return true    
 };
