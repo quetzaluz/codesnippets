10e51bed3162c9412a8d3ca6ae2e96d82f410b22
Cyd La Luz <quetzaluz@gmail.com>
2018-05-08 06:52:50 -0700

    Javascript solution to hackerrank divisible-sum-pairs

---
+++ javascript/hackerrank/divisible-sum-pairs.js
@@ -0,0 +1,49 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+/////////////// ignore above this line ////////////////////
+
+function divisibleSumPairs(n, k, ar) {
+    // Complete this function
+    i0 = 0
+    r = 0
+    while (i0 < n) {
+        i1 = i0 + 1
+        while (i1 < n) {
+            if ((ar[i0] + ar[i1]) % k === 0) {
+                r++
+            }
+            i1++
+        }
+        i0++
+    }
+    return r
+}
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var k = parseInt(n_temp[1]);
+    ar = readLine().split(' ');
+    ar = ar.map(Number);
+    var result = divisibleSumPairs(n, k, ar);
+    process.stdout.write("" + result + "\n");
+
+}
