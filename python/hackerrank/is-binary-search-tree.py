e07c17e18d3a305cb88071b89eaf473afe7a11f7
Cyd La Luz <quetzaluz@gmail.com>
2018-05-10 18:54:08 -0700

    Check for duplicates in python is-binary-search-tree, passes more cases

--- python/hackerrank/is-binary-search-tree.py
+++ python/hackerrank/is-binary-search-tree.py
@@ -6,9 +6,19 @@ class node:
       self.right = None
 """
 
-def check_binary_search_tree_(root):
+def traverse_and_check_(root, seen):
     if root and ((root.left is not None and root.left.data > root.data) or (root.right is not None and root.right.data < root.data)):
         return False
     if root is None:
-        return True
-    return check_binary_search_tree_(root.left) and check_binary_search_tree_(root.right)
+        return seen
+    if root.data in seen:
+        return False
+    seen.append(root.data)
+    seen = traverse_and_check_(root.left, seen)
+    if seen:
+        return traverse_and_check_(root.right, seen)
+    else:
+        return False
+def check_binary_search_tree_(root):
+    seen = []
+    return True if traverse_and_check_(root, seen) else False
