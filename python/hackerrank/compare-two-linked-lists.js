593810e28fd8423482a3c3e3e4f7bd8dda1dfa9a
Cyd La Luz <quetzaluz@gmail.com>
2018-05-13 14:06:53 -0700

    Javascript solution to hackerrank compare-two-linked-lists

---
+++ python/hackerrank/compare-two-linked-lists.js
@@ -0,0 +1,16 @@
+/*
+    Compare two Linked Lists A and B
+    Return 1 if they are identical and 0 if they are not.
+    Node is defined as
+    var Node = function(data) {
+        this.data = data;
+        this.next = null;
+    }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function compareLinkedLists(headA, headB) {
+    
+}
