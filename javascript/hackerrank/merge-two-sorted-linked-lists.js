77fcb5e79e3ce6892f5f6abc99e6c1475cc53ee7
Cyd La Luz <quetzaluz@gmail.com>
2018-05-15 07:08:09 -0700

    Javascript solution to hackerrank merge-two-sorted-linked-lists

---
+++ javascript/hackerrank/merge-two-sorted-linked-lists.js
@@ -0,0 +1,41 @@
+/*
+    Merge two sorted lists A and B as one Linked List and return the head of merged list
+    Node is defined as
+    var Node = function(data) {
+        this.data = data;
+        this.next = null;
+    }
+*/
+
+// This is a "method-only" submission.
+// You only need to complete this method.
+
+function mergeLinkedLists( headA, headB) {
+    let newRoot = new Node()
+    newRoot.next = new Node()
+    let newHead = newRoot.next
+    while (headA || headB) {
+        if (!headA) {
+            newHead.data = headB.data
+            headB = headB.next 
+            newHead.next = new Node()
+            newHead = newHead.next
+        } else if (!headB) {
+            newHead.data = headA.data
+            headA = headA.next 
+            newHead.next = new Node()
+            newHead = newHead.next
+        } else if (headA.data >= headB.data) {
+            newHead.data = headB.data
+            headB = headB.next 
+            newHead.next = new Node()
+            newHead = newHead.next
+        } else if (headB.data >= headA.data) {
+            newHead.data = headA.data
+            headA = headA.next 
+            newHead.next = new Node()
+            newHead = newHead.next
+        }
+    }
+    return newRoot.next
+}
