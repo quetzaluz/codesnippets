b59fa61470dace29c65b5860f413443e1640d060
Cyd La Luz <quetzaluz@gmail.com>
2018-06-01 22:00:19 -0700

    Java solution to leetcode climbing-stairs

---
+++ java/leetcode/climbing-stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        int third = 2; // init as second in case n is small
+        for (int i = 3; i <= n; i++) {
+            third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
+}
