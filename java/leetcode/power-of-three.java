877b1ef3bab04f035536a7569ff76161e44ddd3c
Cyd La Luz <quetzaluz@gmail.com>
2018-06-05 21:03:35 -0700

    Java solution to leetcode power-of-three

---
+++ java/leetcode/power-of-three.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        double divisor = 3.0;
+        double num = (double)n;
+        while (num > divisor) {
+            num /= divisor;
+        }
+        return num == 1 || num == 3;
+    }
+}
