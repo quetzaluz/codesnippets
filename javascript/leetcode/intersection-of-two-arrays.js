ac73159fdf1564bb1a3a055dd9de3ddbaf9f3e14
Cyd La Luz <quetzaluz@gmail.com>
2018-06-20 08:22:23 -0700

    Fixup javascript solution to leetcode intersection-of-two-arrays

--- javascript/leetcode/intersection-of-two-arrays.js
+++ javascript/leetcode/intersection-of-two-arrays.js
@@ -3,11 +3,12 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+
 var intersection = function(nums1, nums2) {
     newArr = []
     for (var i = 0; i < nums1.length; i++) {
         for (var j = 0; j < nums2.length; j++) {
-            if (nums2[j] == nums3[i]) {
+            if (nums2[j] == nums1[i] && newArr.indexOf(nums2[j]) === -1) {
                 newArr.push(nums2[j])
             }
         }
