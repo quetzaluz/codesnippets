fe9e850b3e9cc2b25b97fd8d142c743482cf9bf1
Cyd La Luz <quetzaluz@gmail.com>
2018-07-02 19:44:08 -0700

    Java solution to hackerrank tree-postorder-traversal

---
+++ java/hackerrank/tree-postorder-traversal.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+
+    public static void postOrder(Node root) {
+        // this initialization can be avoided if we had more control over Solution
+        ArrayList<String> result = new ArrayList();
+        result = traverse(root, result);
+        System.out.println(String.join(" ", result));
+    }
+    
+    public static ArrayList<String> traverse(Node root, ArrayList<String> result) {
+        if (root != null) {
+            result = traverse(root.left, result);
+            result = traverse(root.right, result);
+            result.add(Integer.toString(root.data));
+        }
+        return result;
+    }
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        postOrder(root);
+    }	
+}
