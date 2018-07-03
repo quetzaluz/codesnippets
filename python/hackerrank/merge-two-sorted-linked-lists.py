8a3dcfe16ee933ff262a7221bcf9401e220ec64c
Cyd La Luz <quetzaluz@gmail.com>
2018-07-03 07:19:32 -0700

    Fixed up, passing python solution to hackerrank merge-two-sorted-linked-lists

--- python/hackerrank/merge-two-sorted-linked-lists.py
+++ python/hackerrank/merge-two-sorted-linked-lists.py
@@ -1,3 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
 """
  Merge two linked lists
  head could be None as well for empty list
@@ -12,19 +49,54 @@
  return back the head of the linked list in the below method.
 """
 
-def MergeLists(headA, headB):
-	if headA is None and headB is None:
-		return None
-	new_root = Node()
-	new_head = new_root
-	while headA or headB:
-		if headA is None or (headB and headA.data > headB.data):
-			new_head.data = headB.data
-			headB = headB.next
-		elif headB is None or (headB.data > headA.data):
-			new_head.data = headA.data
-			headA = headA.next
-		if headA or headB:
-			new_head.next = Node()
-			new_head = new_head.next
-	return new_root
+def mergeLists(headA, headB):
+    if headA is None and headB is None:
+        return None
+    new_root = SinglyLinkedListNode(0)
+    new_head = new_root
+    while headA or headB:
+        if headA is None or (headB and headA.data > headB.data):
+            new_head.data = headB.data
+            headB = headB.next
+        elif headB is None or (headB.data > headA.data):
+            new_head.data = headA.data
+            headA = headA.next
+        elif headA:
+            new_head.data = headA.data
+            headA = headA.next
+        elif headB:
+            new_head.data = headB.data
+            headB = headB.next
+        if headA or headB:
+            new_head.next = SinglyLinkedListNode(0)
+        new_head = new_head.next
+    return new_root
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist1_count = int(input())
+
+        llist1 = SinglyLinkedList()
+
+        for _ in range(llist1_count):
+            llist1_item = int(input())
+            llist1.insert_node(llist1_item)
+            
+        llist2_count = int(input())
+
+        llist2 = SinglyLinkedList()
+
+        for _ in range(llist2_count):
+            llist2_item = int(input())
+            llist2.insert_node(llist2_item)
+
+        llist3 = mergeLists(llist1.head, llist2.head)
+
+        print_singly_linked_list(llist3, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
