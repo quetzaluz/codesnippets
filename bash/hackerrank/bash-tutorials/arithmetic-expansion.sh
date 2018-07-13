479399da7e368345ab8c5bd4f6312cc8ab97f0a9
Cyd La Luz <quetzaluz@gmail.com>
2018-07-13 07:13:07 -0700

    Solutions to six hackerrank bash tutorial prompts

---
+++ bash/hackerrank/bash-tutorials/arithmetic-expansion.sh
@@ -0,0 +1,7 @@
+# https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers/problem
+read x
+read y
+echo "$(($x + $y))"
+echo "$(($x - $y))"
+echo "$(($x * $y))"
+echo "$(($x / $y))"
