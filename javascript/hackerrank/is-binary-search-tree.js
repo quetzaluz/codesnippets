660e25e89a9f279e23dcc6c3524d94dff2c0f315
Cyd La Luz <quetzaluz@gmail.com>
2018-07-30 06:45:02 -0700

    JavaScript solution to hackerrank is-binary-search-tree

---
+++ javascript/hackerrank/is-binary-search-tree.js
@@ -0,0 +1,19 @@
+/*
+The Node class is defined as follows:
+    class Node {
+    int data;
+    Node left;
+    Node right;
+     }
+*/
+class Solution {
+    static int last = -1; // constraints are data is 0 to 10^4. For checking left of tree
+    boolean checkBST(Node root) {
+        if (root == null) { return true; }
+        if (!checkBST(root.left)) { return false; }
+        if (root.data <= last) { return false; }
+        last = root.data;
+        if (!checkBST(root.right)) { return false; }
+        return true;
+    }
+}
