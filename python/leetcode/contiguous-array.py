d3170edf7b7f03959512857538517804adee6f82
Cyd La Luz <quetzaluz@gmail.com>
2018-08-10 06:52:41 -0700

    O(n) python solution to leetcode contiguous-array

--- python/leetcode/contiguous-array.py
+++ python/leetcode/contiguous-array.py
@@ -5,20 +5,12 @@ class Solution(object):
         :rtype: int
         """
         max_len = 0
-        this_max_len = 0
         balance = 0
-        j = 0
+        results = {0: 0}
         for i in range(len(nums)):
-            this_max_len = 0
-            balance = 0
-            j = i
-            while j < len(nums):
-                balance += -1 if nums[j] == 0 else 1
-                this_max_len += 1
-                if balance == 0 and this_max_len > max_len:
-                    max_len = this_max_len
-                j += 1
-            if balance == 0:
-                if this_max_len > max_len:
-                    max_len = this_max_len
+            balance += -1 if nums[i] == 0 else 1
+            if balance in results:
+                max_len = max(max_len, i + 1 - results[balance])
+            else:
+                results[balance] = i + 1
         return max_len
