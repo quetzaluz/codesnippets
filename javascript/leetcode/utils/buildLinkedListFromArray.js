f3904df940d2f6edf2dfbf49a7e0fd750f857852
Cyd La Luz <quetzaluz@gmail.com>
2018-08-17 08:39:03 -0700

    Update util for building linked list from array to remove redundancy and include class definition for ListNode

--- javascript/leetcode/utils/buildLinkedListFromArray.js
+++ javascript/leetcode/utils/buildLinkedListFromArray.js
@@ -1,14 +1,16 @@
+class ListNode {
+  constructor (val) {
+    this.val = val,
+    this.next = null,
+  }
+}
+
 var buildLinkedListFromArray = function (arr) {
   var head = new ListNode(arr[0]) // initialize first node
-  var currentNode;
+  var currentNode = head;
   for (var i = 1; i < arr.length; i++) {
-    if (currentNode) {
       currentNode.next = new ListNode(arr[i])
       currentNode = currentNode.next
-    } else {
-      head.next = new ListNode(arr[i])
-      currentNode = head.next
-    }
   }
   return head
 }
