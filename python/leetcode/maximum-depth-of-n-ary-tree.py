fa2088bd89a133e35b85f6db0d194db1b96eb718
Cyd La Luz <quetzaluz@gmail.com>
2018-08-22 08:13:54 -0700

    Pythjon solution to leetcode maximum-depth-of-n-ary-tree

---
+++ python/leetcode/maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,18 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        depths = map(self.maxDepth, root.children)
+        depths.append(0) # default for max function
+        return max(depths) + 1
