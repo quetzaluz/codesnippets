0560cf38bf809f5137a74f69d91e3613fc5623cc
Cyd La Luz <quetzaluz@gmail.com>
2018-09-20 21:59:14 -0700

    JavaScript solution to leetcode symmetric-tree

---
+++ javascript/leetcode/symmetric-tree.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if (root === null) {
+        return true;
+    }
+    return isSameTree(
+        invertTree(root.left),
+        root.right
+    );
+};
+
+var isSameTree = function(p, q) {
+    if (p == null && q == null) { return true }
+    if (p == null && q != null || q == null && p != null) { return false }
+    if (p.val !== q.val) { return false }
+    if (p.val == q.val) {
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+    }
+};
+
+var invertTree = function(root) {
+    if (!root) { return root}
+    tmpNode = root.left
+    root.left = root.right
+    root.right = tmpNode
+    if (root.left) { root.left = invertTree(root.left) }
+    if (root.right) { root.right = invertTree(root.right) }
+    return root
+};
