9a4c2c3e80e1aa5891e1e826c6484c39436513d3
Cyd La Luz <quetzaluz@gmail.com>
2018-09-21 20:03:45 -0700

    ruby solution to leetcode h-index

---
+++ python/leetcode/h-index.rb
@@ -0,0 +1,11 @@
+# @param {Integer[]} citations
+# @return {Integer}
+def h_index(citations)
+    ans = 0
+    citations.sort.reverse!.each_with_index do |c, i|
+        if i < c
+            ans += 1
+        end
+    end
+    return ans
+end
