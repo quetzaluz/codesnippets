eeb29a13f6d32ebbe1d4490b196a1f3bf69c9178
Cyd La Luz <quetzaluz@gmail.com>
2018-09-24 06:35:12 -0700

    Java solution to leetcode h-index-ii

---
+++ java/leetcode/h-index-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        int ans = 0;
+        for (int i = citations.length - 1; i >= 0; i--) {
+            if (citations.length - i - 1 < citations[i]) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
