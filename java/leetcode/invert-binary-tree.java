8406f225253c49c33dc58e7f32cf8756f8dc1a6f
Cyd La Luz <quetzaluz@gmail.com>
2018-09-26 08:20:36 -0700

    Remove unneeded conditionals from existing leetcode invert-binary-tree solutions (python, javascript, java)

--- java/leetcode/invert-binary-tree.java
+++ java/leetcode/invert-binary-tree.java
@@ -13,8 +13,8 @@ class Solution {
         TreeNode tmpNode = root.left;
         root.left = root.right;
         root.right = tmpNode;
-        if (root.left != null) { root.left = invertTree(root.left); }
-        if (root.right!= null) { root.right = invertTree(root.right); }
+        root.left = invertTree(root.left);
+        root.right = invertTree(root.right);
         return root;
     }
 }
--- javascript/leetcode/invert-binary-tree.js
+++ javascript/leetcode/invert-binary-tree.js
@@ -10,11 +10,11 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    if (!root) { return root}
+    if (!root) { return root }
     tmpNode = root.left
     root.left = root.right
     root.right = tmpNode
-    if (root.left) { root.left = invertTree(root.left) }
-    if (root.right) { root.right = invertTree(root.right) }
+    root.left = invertTree(root.left)
+    root.right = invertTree(root.right)
     return root
 };
