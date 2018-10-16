b1ed5ed895839cf21acb28d5ad92f03032fedc3c
Cyd La Luz <quetzaluz@gmail.com>
2018-10-16 07:36:19 -0700

    Updated solution to hackerrank python diagonal-difference per new prompt

--- python/hackerrank/diagonal-difference.py
+++ python/hackerrank/diagonal-difference.py
@@ -1,43 +1,41 @@
 #!/bin/python3
 
+import math
 import os
+import random
+import re
 import sys
 
-#
 # Complete the diagonalDifference function below.
-#
-def diagonalDifference(a):
-    #
-    # Write your code here.
-    #
-    firstSum = 0
-    secondSum = 0
-    c = 0
+def diagonalDifference(arr):
     r = 0
-    while c < len(a):
-        firstSum += a[c][r]
+    c = 0
+    sum1 = 0
+    sum2 = 0
+    while r < len(arr):
+        sum1 += arr[r][c]
         c += 1
         r += 1
-    r = 0
-    c = len(a) - 1
-    while c >= 0:
-        secondSum += a[c][r]
-        c -= 1
-        r += 1
-    return abs(secondSum - firstSum)
+    c = 0
+    r -= 1
+    while r >= 0:
+        sum2 += arr[r][c]
+        c += 1
+        r -= 1
+    return abs(sum2 - sum1)
 
 if __name__ == '__main__':
-    f = open(os.environ['OUTPUT_PATH'], 'w')
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     n = int(input())
 
-    a = []
+    arr = []
 
     for _ in range(n):
-        a.append(list(map(int, input().rstrip().split())))
+        arr.append(list(map(int, input().rstrip().split())))
 
-    result = diagonalDifference(a)
+    result = diagonalDifference(arr)
 
-    f.write(str(result) + '\n')
+    fptr.write(str(result) + '\n')
 
-    f.close()
+    fptr.close()
