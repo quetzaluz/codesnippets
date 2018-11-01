613573b95a60bafbc5cd637fa8263da3926cf41d
Cyd La Luz <quetzaluz@gmail.com>
2018-11-01 06:27:54 -0700

    python solution to hackerrank designer-pdf-viewer

---
+++ python/hackerrank/designer-pdf-viewer.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the designerPdfViewer function below.
+def designerPdfViewer(h, word):
+    max_height = 0
+    for char in word:
+        pos = ord(char) - 97
+        max_height = max(h[pos], max_height)
+    return max_height * len(word)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
