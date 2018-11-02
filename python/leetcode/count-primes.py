717fb5f9c87ed184d65bad72635f31a25346f91b
Cyd La Luz <quetzaluz@gmail.com>
2018-11-02 06:56:19 -0700

    Modest (~400ms) runtime improvement to python leetcode count-primes solution

--- python/leetcode/count-primes.py
+++ python/leetcode/count-primes.py
@@ -1,5 +1,4 @@
 class Solution:
-    # Still takes a very long time on large inputs
     def countPrimes(self, n):
         """
         :type n: int
@@ -10,25 +9,6 @@ class Solution:
         for i in range(2, n):
             if non_prime[i] == False:
                 prime_count += 1
-                j = 2
-                while i * j < n:
+                for j in range(2, (n - 1) // i + 1):
                     non_prime[i * j] = True
-                    j += 1
-        return prime_count
-
-    # Very naive brute force solution
-    def countPrimesBruteForce(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        prime_count = 0
-        for i in range(2, n):
-            is_prime = True
-            for k in range(2, i - 1):
-                if i % k == 0:
-                    is_prime = False
-                    break
-            if is_prime:
-                prime_count += 1
         return prime_count
