06ce2d38b432670dd66e5e149af97b34bce64f4b
Cyd La Luz <quetzaluz@gmail.com>
2019-08-23 06:45:13 -0700

    in place solution to leetcode increasing-order-search-tree (javascript)

--- javascript/leetcode/increasing-order-search-tree.js
+++ javascript/leetcode/increasing-order-search-tree.js
@@ -10,29 +10,16 @@
  * @return {TreeNode}
  */
 const increasingBST = function(root) {
-    const values = traverse(root, []);
-    return buildNewTree(values);
-};
-
-const traverse = function(root, values) {
-    if (root === null) { return values }
-    values = traverse(root.left, values);
-    values.push(root.val);
-    values = traverse(root.right, values);
-    return values;
-}
-
-const buildNewTree = function(values) {
-    const newRoot = new TreeNode();
-    let root = newRoot;
-    let i = 0;
-    while (i < values.length) {
-        root.val = values[i];
-        if (i < values.length - 1) {
-            root.right = new TreeNode();
-            root = root.right;
-        }
-        i++;
+    const head = new TreeNode();
+    let curr = head;
+    const traverse = function(root) {
+        if (root === null) { return }
+        traverse(root.left);
+        curr.right = root;
+        curr = root
+        curr.left = null;
+        traverse(root.right);
     }
-    return newRoot;
-}
+    traverse(root);
+    return head.right;
+};
