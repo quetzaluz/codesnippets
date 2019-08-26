9330fd35ed33227d415c1f64b8b408efb2af5204
Cyd La Luz <quetzaluz@gmail.com>
2019-08-26 07:07:45 -0700

    Maintain both value list and relinking solution for increasing-order-search-tree since both are valid

--- javascript/leetcode/increasing-order-search-tree.js
+++ javascript/leetcode/increasing-order-search-tree.js
@@ -10,6 +10,39 @@
  * @return {TreeNode}
  */
 const increasingBST = function(root) {
+    const values = traverse(root, []);
+    return buildNewTree(values);
+};
+
+const traverse = function(root, values) {
+    if (root === null) { return values }
+    values = traverse(root.left, values);
+    values.push(root.val);
+    values = traverse(root.right, values);
+    return values;
+}
+
+const buildNewTree = function(values) {
+    const newRoot = new TreeNode();
+    let root = newRoot;
+    let i = 0;
+    while (i < values.length) {
+        root.val = values[i];
+        if (i < values.length - 1) {
+            root.right = new TreeNode();
+            root = root.right;
+        }
+        i++;
+    }
+    return newRoot;
+}
+
+
+/** Alternative relinking solution
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+const increasingBST = function(root) {
     const head = new TreeNode();
     let curr = head;
     const traverse = function(root) {
