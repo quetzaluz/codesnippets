bf34106e464c2eaaa6267153378a596cd6badf7f
Cyd La Luz <quetzaluz@gmail.com>
2019-08-29 07:37:58 -0700

    alternate python solution to kth-smallest-element-in-a-bst

--- python/leetcode/kth-smallest-element-in-a-bst.py
+++ python/leetcode/kth-smallest-element-in-a-bst.py
@@ -12,3 +12,15 @@ class Solution:
         if root is None:
             return []
         return self.traverse(root.left) + [root.val] + self.traverse(root.right)
+
+# Can also be written as such with arr passed in
+
+# class Solution:
+#     def kthSmallest(self, root: TreeNode, k: int) -> int:
+#         return self.traverse(root, [])[k - 1]
+#     def traverse(self, root, arr):
+#         if root is not None:
+#             self.traverse(root.left, arr)
+#             arr.append(root.val)
+#             self.traverse(root.right, arr)
+#         return arr
